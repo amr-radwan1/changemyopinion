2653dfc4c170d61bfff5fdfff56ab27211351239
pwatana <patricia.watanabe@mail.utoronto.ca>
2025-01-18 02:04:47 -0500

    small change

--- opinions_app/urls.py
+++ opinions_app/urls.py
@@ -1,12 +1,12 @@
 # opinions_app/urls.py
 from django.urls import path
 from .views import PostListView
-from .views import LoginView
+from .views import login_user
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='post-list'),  # API endpoint for getting all posts
 ]
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', login_user.as_view(template_name='users/login.html'), name='login'),
 ]
