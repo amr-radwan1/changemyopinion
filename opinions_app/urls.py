fa7d0bf1601723a6708e127fd3a072bfb1db929e
amr-radwan1 <amrradwan2006@gmail.com>
2025-01-18 10:10:22 -0500

    login url missing, allowed host

--- opinions_app/urls.py
+++ opinions_app/urls.py
@@ -1,6 +1,7 @@
 # opinions_app/urls.py
 from django.urls import path
-from .views import UserListView, PostListView, FollowerListView, PromptListView, LogoutUserView, UpdateVotesView, RegisterUserView, GetRepliesView
+from .views import UserListView, PostListView, FollowerListView, PromptListView, LogoutUserView
+from .views import UpdateVotesView, RegisterUserView, GetRepliesView, LoginUserView
 
 urlpatterns = [
     path('users/', UserListView.as_view(), name='user-list'),
@@ -8,6 +9,7 @@ urlpatterns = [
     path('users/<int:userid>/followers/', FollowerListView.as_view(), name='follower-list'),
     path('prompts/', PromptListView.as_view(), name='prompt-list'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
+    path('login/', LogoutUserView.as_view(), name='login'),
     path('posts/<int:post_id>/<str:vote_type>/', UpdateVotesView.as_view(), name='update-votes'),
     path('register/', RegisterUserView.as_view(), name='register'),
     path('posts/<int:post_id>/replies', GetRepliesView.as_view(), name='get_replies')
